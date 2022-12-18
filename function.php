0e8fbd7d286db90400d327c347eaf1822b60a488
radoslavnaya-coder <killaarte13@gmail.com>
2022-12-19 03:38:39 +0400

    finally

--- function.php
+++ function.php
@@ -12,27 +12,29 @@ function getPosts($connect){
         echo json_encode($postlist);
     }
 
-function getPost($connect, $id){
-    $post = mysqli_query($connect, "SELECT * FROM `posts` WHERE `id` = '$id'");
-    if(mysqli_num_rows($post) === 0){
-        http_response_code(404);
-        $res = [
-            "status" => false,
-            "message" => "Post not found"
-        ];
-        echo json_encode($res);
-    }
-    else{
-        $post = mysqli_fetch_assoc($post);
-        echo json_encode($post);
-    }
-}
+//function getPost($connect, $id){
+//    $post = mysqli_query($connect, "SELECT * FROM `posts` WHERE `id` = '$id'");
+//    if(mysqli_num_rows($post) === 0){
+//        http_response_code(404);
+//        $res = [
+//            "status" => false,
+//            "message" => "Post not found"
+//        ];
+//        echo json_encode($res);
+//    }
+//    else{
+//        $post = mysqli_fetch_assoc($post);
+//        echo json_encode($post);
+//    }
+//}
 
 function addPost($connect, $data){
-    mysqli_query($connect, "INSERT INTO `posts` (`id`, `title`, `body`) VALUES (NULL, '$title', '$body') ");
-    http_response_code(201);
     $title = $data['title'];
     $body = $data['body'];
+   
+    mysqli_query($connect, "INSERT INTO `posts` (`id`, `title`, `body`) VALUES (NULL, '$title', '$body') ");
+
+    http_response_code(201);
     $res = [
         "status" => true,
         "post_id" => mysqli_insert_id($connect)
@@ -52,7 +54,7 @@ function updatePost($connect, $id, $data){
     echo json_encode($res);
 }
 
-function seletePost($connect,$id){
+function deletePost($connect,$id){
     mysqli_query($connect, "DELETE FROM `posts` WHERE `posts`.`id` = '$id'");
     http_response_code(200);
     $res = [
--- indexapi.php
+++ indexapi.php
@@ -1,4 +1,9 @@
 <?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: *');
+    header('Access-Control-Allow-Credentials: true');
+
     header('Content-type: json/application');
     require 'connect.php';
     require 'function.php';
@@ -7,7 +12,9 @@
         $q = $_GET['q'];
         $params = explode('/',$q);
         $type = $params[0];
-        $id = $params[1];
+        if(isset($params[1])){
+            $id = $params[1];
+        }
 
         if($method === 'GET'){
             if($type === 'posts'){
